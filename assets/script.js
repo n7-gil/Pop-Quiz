17c227d64b999f5a9e1f1228d0f44a5b335f23d1
N7 GIL <gilbertoaescobedo@gmail.com>
2022-06-10 21:58:25 -0700

    added container class in index and html for a starting timer. adjusted margins and padding on stylesheet. Began working on starting timer

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,57 @@
-function generateQuiz(questions, quizContainer, resultContainer, submitButton);
+var timerEl = document.getElementById('start');
+var mainEl = document.getElementById('main');
 
-function showQuestions(questions, quizContainer) {
-// code WIP 
 
+
+
+// Beginning sequence 
+function startCountdown() {
+    var startTimer = 5;
+
+    var timerInterval = setInterval(function () {
+        if (startTimer > 1) {
+            timerEl.textContent = "Pop quiz begins in " + startTimer;
+            startTimer--;
+
+
+
+        } else if (secondsLeft === 0) {
+            timerEl.textContent = startTimer + ' seconds remaining';
+            startTimer--;
+        } else {
+            timerEl.textContent = '';
+            clearInterval(timerInterval);
+        }
+     }, 1000);
 }
+startCountdown()
 
-function showResults(questions)
+
+
+console.log(startCountdown);
+
+
+
+
+// var timerEl = document.getElementById('countdown');
+// var mainEl = document.getElementById("main")
+
+
+
+
+
+
+// var quizQuestions
+// var quizContainer
+// var resultContainer
+// var submitButton
+
+
+// function generateQuiz(questions, quizContainer, resultContainer, submitButton);
+
+// function showQuestions(questions, quizContainer) {
+// // // code WIP
+
+// // }
+
+// // function showResults(questions)
