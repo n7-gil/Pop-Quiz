8aebeda160614e54355a0a341a85c6c0c65b62e7
N7 GIL <gilbertoaescobedo@gmail.com>
2022-06-14 18:31:32 -0700

    Edited readMe file. Also worked on js code. Fixing functionality of having the questions be able to move on to the next question after one was answered

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Pop-Quiz
+
+The purpose of this assignment was to create a fully functional quiz game. The game begins when the user selects the start button and must answer trivia questions before the timer expires. The game is still a work in progress as I will add a function that will punish the user when selecting an incorrect answer and will subtract time from their remaining time. Also need to add a leaderboard in the end so users can play against friends to compare how quickly they can solve the quiz.
+
+[Pop-Quiz]: https://github.com/n7-gil/Pop-Quiz
+
+![Screenshot of completed web page.](assets/screenshot.PNG)
--- assets/script.js
+++ assets/script.js
@@ -1,106 +1,128 @@
 var beginEl = document.getElementById("start");
 var timerEl = document.getElementById("timer");
 var mainEl = document.getElementById("card");
-var questionEl = document.getElementById("questions")
+var timeLeft = 90;
+var questionEl = document.getElementById("questions");
+var submitEl = document.getElementById("submit");
+var resultEl = document.getElementById("result");
+var questionNum = 0
 var quizQuestions = [
   {
-    question: "What is this question?",
+    question: "A variable is a name of a storage location",
     answers: {
       a: "True",
       b: "False",
     },
-    correctAnswer: 'a'
+    correctAnswer: 'True'
   },
   {
-    question: "What is question number two?",
+    question: "console.log(); writes a message to the console",
     answers: {
       a: "True",
       b: "False",
     },
-    correctAnswer: 'a'
+    correctAnswer: 'True'
   },
   {
-    question: "What is question number three?",
+    question: "A boolean is another form of a string",
     answers: {
       a: "True",
       b: "False",
     },
-    correctAnswer: 'b'
+    correctAnswer: 'False'
   },
   {
-    question: "What is question number four?",
+    question: "A function is a block of code designed to perform a specific task.",
     answers: {
       a: "True",
       b: "False",
     },
-    correctAnswer: 'a'
+    correctAnswer: 'True'
   },
   {
-    question: "What is question number five?",
+    question: "mathrandom.(); returns a random number from 0(inclusive) up to but not including 1(exclusive)",
     answers: {
       a: "True",
       b: "False",
     },
-    correctAnswer: 'b'
+    correctAnswer: 'False'
   }
 ]
 
 console.log(quizQuestions);
 
 // once the begin button is clicked
-beginEl.addEventListener("click", startGame) 
+beginEl.addEventListener("click", startGame)
 
 // the button itself dissapears, the main card appears, and the timer begins 
-function startGame(){
-    // console.log("startGame");
-    beginEl.classList.add('hide');
-    mainEl.classList.remove('hide');
-    document.getElementById("questions").textContent= quizQuestions[0]['question'];
-    document.getElementById("answer-a").textContent=quizQuestions[0]['answers']['a'];
-    document.getElementById("answer-b").textContent=quizQuestions[0]['answers']['b'];
-    setTime();
+function startGame() {
+  // console.log("startGame");
+
+  var answerA = document.getElementById("answer-a");
+  var answerB = document.getElementById("answer-b");
+
+  beginEl.classList.add('hide');
+  mainEl.classList.remove('hide');
+  document.getElementById("questions").textContent = quizQuestions[questionNum]['question'];
+  answerA.textContent = quizQuestions[questionNum]['answers']['a'];
+  answerB.textContent = quizQuestions[questionNum]['answers']['b'];
+  setTime();
+
+  answerA.addEventListener("click", answerQuestion);
+  answerB.addEventListener("click", answerQuestion);
 }
+
 // time remaining function 
 function setTime() {
-    var timeLeft = 90;
-  
-    var timeInterval = setInterval(function () {
-      if (timeLeft > 1) {
-        timerEl.textContent = timeLeft + ' seconds remaining';
-        timeLeft--;
 
-      } else if (timeLeft === 1) {
-        timerEl.textContent = timeLeft + ' second remaining';
-        timeLeft--;
 
-      } else {
-        // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-        timerEl.textContent = '';
-        clearInterval(timeInterval);
-      }
-    }, 1000);
-  }
+  var timeInterval = setInterval(function () {
+    if (timeLeft > 1) {
+      timerEl.textContent = timeLeft + ' seconds remaining';
+      timeLeft--;
 
-  function answerQuestion() {
-    let userAnswer = answer.textContent;
-    let correctAnswer = questions.answer;
+    } else if (timeLeft === 1) {
+      timerEl.textContent = timeLeft + ' second remaining';
 
-    if (userAnswer === correctAnswer) {
-      userScore += 1;
-      answer.classList.add("Correct"); 
 
-    }else{
-      answer.classList.add("Incorrect");
-      timeleft = timeLeft -20
+    } else {
+      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+      timerEl.textContent = '';
+      clearInterval(timeInterval);
     }
-  }
-
-  
-function nextQuestion() {
-    questionlist.textContent = questions[questionIndex].question;
-    button1.textContent = questions[questionIndex].answers[0].text
-    button2textContent = questions[questionIndex].answers[1].text
-
-    selectAnswer()
+  }, 1000);
 }
+
+function answerQuestion(e) {
+  var userAnswer = e.target.textContent;
+  var correctAnswer = quizQuestions[questionNum]['correctAnswer'];
+
+  //  console.log(userAnswer);
+  //  console.log(correctAnswer);
+
+  if (userAnswer === correctAnswer) {
+    resultEl.textContent = "correct";
+
+  } else {
+    resultEl.textContent = "incorrect";
+    //     timeleft = timeLeft - 20000;
+    //     timerEl.textContent = timeLeft + ' - seconds'
+  }
+  setTimeout(function () {
+    nextQuestion()
+    resultEl.textContent = "";
+  }, 1000);
+
+}
+
+// once submit button is clicked 
+submitEl.addEventListener("click", nextQuestion)
+// moves on to next question 
+function nextQuestion() {
+  questionNum += 1;
+  document.getElementById("questions").textContent = quizQuestions[questionNum]['question'];
+  document.getElementById("answer-a").textContent = quizQuestions[questionNum]['answers']['a'];
+  document.getElementById("answer-b").textContent = quizQuestions[questionNum]['answers']['b'];
+}
+
 
